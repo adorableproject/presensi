3ef8fd93c9b6aa9f90ec3b9e7be3da9fa54646a1
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 01:55:25 +0700

    cobain

--- js/croot.js
+++ js/croot.js
@@ -2,10 +2,12 @@ import { get } from "https://jscroot.github.io/api/croot.js";
 import { setInner } from "https://jscroot.github.io/element/croot.js";
 
 let URLPresensi = "https://gocroot.herokuapp.com/presensi";
+hasil={};
 
 get(URLPresensi,isiTablePresensi);
 
 function isiTablePresensi(results){
     console.log(results);
+    hasil=results;
 }
 setInner("namadivisi","Dari croot.js");
