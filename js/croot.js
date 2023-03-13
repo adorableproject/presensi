40da7ef5e342eced1508f5e3e4e3ac0dcf8b0955
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 05:09:48 +0700

    console log dulu

--- js/croot.js
+++ js/croot.js
@@ -1,6 +1,8 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { setInner, addInner } from "https://jscroot.github.io/element/croot.js";
+import {getRandomColor} from "https://jscroot.github.io/image/croot.js";
 import {tr} from "./template/table.js"
+
 
 let URLPresensi = "https://gocroot.herokuapp.com/presensi";
 
@@ -11,8 +13,9 @@ function isiTablePresensi(results){
 }
 
 function isiRow(value){
-    let row=tr.replace("#NAMA#",value.Biodata.Nama).replace("#PHONENUMBER#",value.Phone_number).replace("#LOKASI#",value.Location).replace("#KET#",value.Checkin).replace("#MASUK#",value.Datetime).replace("#PULANG#",value.Datetime).replace("#DURASI#",value.Datetime);
-    addInner("karyawan",row)
+    let row=tr.replace("#NAMA#",value.Biodata.Nama).replace("#PHONENUMBER#",value.Phone_number).replace("#LOKASI#",value.Location).replace("#KET#",value.Checkin).replace("#MASUK#",value.Datetime).replace("#PULANG#",value.Datetime).replace("#DURASI#",value.Datetime).replace("#WARNA#",getRandomColor());
+    console.log(row)
+    //addInner("karyawan",row)
 }
 
 
