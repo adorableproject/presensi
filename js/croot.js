db6e5665fb4b916d8b3b589e46b221e7c157718f
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 06:23:53 +0700

    trtag

--- js/croot.js
+++ js/croot.js
@@ -1,7 +1,7 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { setInner, addInner } from "https://jscroot.github.io/element/croot.js";
 import {getRandomColor,getRandomColorName} from "https://jscroot.github.io/image/croot.js";
-import {tr} from "./template/table.js"
+import {trcontent,trtag} from "./template/table.js"
 
 
 let URLPresensi = "https://gocroot.herokuapp.com/presensi";
@@ -13,9 +13,10 @@ function isiTablePresensi(results){
 }
 
 function isiRow(value){
-    let row=tr.replace("#NAMA#",value.Biodata.Nama).replace("#PHONENUMBER#",value.Phone_number).replace("#LOKASI#",value.Location).replace("#KET#",value.Checkin).replace("#MASUK#",value.Datetime).replace("#PULANG#",value.Datetime).replace("#DURASI#",value.Datetime).replace("#WARNA#",getRandomColor()).replace(/#WARNALOGO#/g,getRandomColorName());
-    console.log(row)
-    addInner("karyawan",row)
+    let row=trcontent.replace("#NAMA#",value.Biodata.Nama).replace("#PHONENUMBER#",value.Phone_number).replace("#LOKASI#",value.Location).replace("#KET#",value.Checkin).replace("#MASUK#",value.Datetime).replace("#PULANG#",value.Datetime).replace("#DURASI#",value.Datetime).replace("#WARNA#",getRandomColor()).replace(/#WARNALOGO#/g,getRandomColorName());
+    trtag.innerHTML = row 
+    console.log(trtag)
+    document.getElementById('karyawan').appendChild(trtag);
 }
 
 
