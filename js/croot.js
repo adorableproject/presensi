5a0c0c108a214affbefb3077a6611792ec11a424
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 11:23:16 +0700

    rapihisasi

--- js/croot.js
+++ js/croot.js
@@ -1,15 +1,8 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
-import {setInner, addChild } from "https://jscroot.github.io/element/croot.js";
-import {getRandomColor,getRandomColorName} from "https://jscroot.github.io/image/croot.js";
-import {presensiTag,presensiClass,presensiContent} from "./template/table.js"
-import {isiTablePresensi} from "./controller/table.js"
-import {URLPresensi} from "./config/url.js"
-
-
+import {setInner } from "https://jscroot.github.io/element/croot.js";
+import {isiTablePresensi} from "./controller/table.js";
+import {URLPresensi} from "./config/url.js";
 
 get(URLPresensi,isiTablePresensi);
-
-
-
 
 setInner("namadivisi","Dari croot.js");
