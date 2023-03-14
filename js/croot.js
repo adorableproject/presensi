0ce73587c7555a51bf4e5cb0cd1408b42caae0a7
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 10:55:12 +0700

    setInner

--- js/croot.js
+++ js/croot.js
@@ -1,5 +1,5 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
-import { addChild } from "https://jscroot.github.io/element/croot.js";
+import {setInner, addChild } from "https://jscroot.github.io/element/croot.js";
 import {getRandomColor,getRandomColorName} from "https://jscroot.github.io/image/croot.js";
 import {presensiTag,presensiClass,presensiContent} from "./template/table.js"
 
